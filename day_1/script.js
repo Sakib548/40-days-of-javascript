610386b76a91b10e55d5a0f93d4f5a811dfd970e
Md Sakib Hassan <mdsakibhassan548@gmail.com>
2026-04-15 16:54:45 +0600

    first-assignment

---
+++ day_1/script.js
@@ -0,0 +1,7 @@
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+document.write("I am a software engineer");
+
+const heading = document.getElementById("someID");
+
+heading.innerText = "Hello";
